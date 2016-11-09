d26efa2d6d9e68d4911554d0e73c8da585683175
Nakasar <kevin.thizy@outlook.fr>
2016-11-09 15:27:19 +0100

    Deleting debug println in test set

--- src/abrSE/testABR.java
+++ src/abrSE/testABR.java
@@ -35,8 +35,8 @@ public class testABR extends TestCase {
 		tree.insert(1);
 		tree.insert(4);
 		tree.insert(9);
+		tree.insert(9);
 		tree.insert(8);
-		System.out.println(tree.toList().toString());
-		assertEquals(tree.toList().toString(), "[1, 4, 5, 8, 9]");
+		assertEquals(tree.toList().toString(), "[1, 4, 5, 8, 9, 9]");
 	}
 }
